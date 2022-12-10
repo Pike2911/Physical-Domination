562e2c0b62983b88031ed8788fe71ca5a027f487
DESKTOP-23SR2A8\piage <piaget2911@hotmail.com>
2022-12-10 12:07:08 +0700

    Open and close phone fix superjunp

---
+++ Assets/Assets/Script/Usephone.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usephone : MonoBehaviour
+{
+    [SerializeField] private GameObject phone;
+    
+    private void Start()
+    {
+            
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (phone.activeSelf != true)
+            {
+                    phone.SetActive(true);
+            }
+            else
+            {
+                    phone.SetActive(false);
+            }
+        }
+        
+        
+    }
+}
